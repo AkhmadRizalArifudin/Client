ffa08e53fc8d47616fdf74f4b7bf33a8a8ece412
AkhmadRizalArifudin <ararifudin@gmail.com>
2022-06-15 21:41:40 +0700

    second

--- app/src/main/java/com/example/nitishkumar/socketchat/MainActivity.java
+++ app/src/main/java/com/example/nitishkumar/socketchat/MainActivity.java
@@ -76,12 +76,15 @@ public class MainActivity extends AppCompatActivity {
         mSocket.on("typing",onTyping);
         mSocket.on("stop typing",onStopTyping);
     }
+
+    //sign request ke loginactivity
     private void signIn(){
         mUsername=null;
         Intent i=new Intent(this,LoginActivity.class);
         startActivityForResult(i,REQUEST_CODE);
     }
 
+    //result login diproses
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
